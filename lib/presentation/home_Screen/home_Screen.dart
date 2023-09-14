c862dbcc3430365131e47e8cda6b13a25e7425c8
Rohit <rohit98waghmare@gmail.com>
2023-09-14 21:30:13 +0530

    home Screen Update

--- lib/presentation/home_Screen/home_Screen.dart
+++ lib/presentation/home_Screen/home_Screen.dart
@@ -12,11 +12,46 @@ class _home_ScreenState extends State<home_Screen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            child: Container(
+              height: 170,
+              decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50))),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 130, right: 15, left: 15),
+            child: Positioned(
+              child: Container(
+                height: 800,
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 253, 253, 253),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(50),
+                        topRight: Radius.circular(50))),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, 50, 0, 0),
+            child: Text("Aurangabad",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.w700)),
+          ),
+        ],
+      ),
       bottomNavigationBar: Padding(
         padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
         child: Container(
           padding: EdgeInsets.all(10),
-          // height: 0,
           decoration: BoxDecoration(
               color: Colors.green, borderRadius: BorderRadius.circular(20)),
           child: GNav(
